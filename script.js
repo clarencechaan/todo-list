653040d4975dd926697c1d9774c0c60990b484c0
Clarence Chan <clarencechan123@gmail.com>
2021-08-24 22:31:07 -0400

    added check/uncheck functionality and remove todo functionality

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let projects = []
+let todoID = 0;
+let projID = 0;
 
 class Todo {
-    constructor(title, description, dueDate, priority, isComplete) {
+    constructor(title, description, dueDate, priority, isComplete, id) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.isComplete = isComplete;
+        this.id = id;
     }
 }
 
@@ -19,14 +22,29 @@ class Project {
 
 function writeTodoToDOM(todo) {
     const d = document.createElement('div');
-    d.innerHTML = todo.title + ": ";
+    const completedChk = document.createElement("INPUT");
+    completedChk.setAttribute("type", "checkbox");
+
+    d.innerHTML = "";
+
+    if (todo.isComplete) {
+        completedChk.setAttribute("checked", true);
+    }
+    completedChk.setAttribute("onclick", `toggleCompleted(${todo.id});`);
+
+    d.appendChild(completedChk);
+    d.innerHTML += todo.title + ": ";
     d.innerHTML += todo.description + ", ";
     d.innerHTML += todo.dueDate + ", ";
     d.innerHTML += "Priority: " + todo.priority + ", ";
-    d.innerHTML += "Completed: " + todo.isComplete;
-    
+    d.innerHTML += "Completed: ";
 
-    console.log(d);
+    const removeBtn = document.createElement('button');
+    removeBtn.innerHTML = "Remove";
+    removeBtn.setAttribute("onclick", `removeTodo(${todo.id}); reloadTodos()`);
+    d.appendChild(removeBtn);
+    
+    d.id = todo.id;
 
     const content = document.getElementById("todo-list");
     content.appendChild(d);
@@ -43,9 +61,9 @@ function addTodoFromForm() {
     const description = document.forms["todo-input"]["description"].value;
     const date = document.forms["todo-input"]["date"].value;
     const priority = document.forms["todo-input"]["priority"].value;
-    const todo = new Todo(title, description, date, priority, false);
+    const todo = new Todo(title, description, date, priority, false, todoID, projID);
+    todoID++;
     projects[0].todos.push(todo);
-    console.log(projects);
 }
 
 function clearTodoListFromDOM() {
@@ -53,8 +71,45 @@ function clearTodoListFromDOM() {
     todoList.innerHTML = '';
 }
 
-function writeProjectListToDOM() {
+function clickNewTodo() {
+    console.log("new todo clicked");
+}
 
+function toggleCompleted(id){
+    projects[projID].todos.find(todo => todo.id === id).isComplete 
+        = !projects[projID].todos.find(todo => todo.id === id).isComplete ;
+}
+
+function removeTodo(id) {
+    const todos = projects[projID].todos;
+
+    let i = 0
+    while (i < todos.length) {
+        if (todos[i].id === id) {
+            break;
+        }
+        i++;
+    }
+    projects[projID].todos.splice(i, 1);
+}
+
+function reloadTodos() {
+    clearTodoListFromDOM(); 
+    writeProjectToDOM(projects[projID]);
+}
+
+function createNewProject() {
+    const projectTitle = prompt("Enter name for your new project");
+    const p = new Project([], projectTitle);
+    projects.push(p);
+    addProjectToDOM(projectTitle);
+}
+
+function addProjectToDOM(projectTitle) {
+    const selectProjects = document.querySelector('#projects');
+    const projectOption = document.createElement('option');
+    projectOption.innerHTML = projectTitle;
+    selectProjects.appendChild(projectOption);
 }
 
 const p = new Project([], "Default project");
